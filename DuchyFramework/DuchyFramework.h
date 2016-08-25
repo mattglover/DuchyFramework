2cb29e76773bfd55af5aa4a8e3d9afaa4fab2770
Matt Glover <matt@duchysoftware.com>
2016-08-25 15:13:47 +0100

    Initial Commit

---
+++ DuchyFramework/DuchyFramework.h
@@ -0,0 +1,19 @@
+//
+//  DuchyFramework.h
+//  DuchyFramework
+//
+//  Created by Matt Glover on 25/08/2016.
+//  Copyright © 2016 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DuchyFramework.
+FOUNDATION_EXPORT double DuchyFrameworkVersionNumber;
+
+//! Project version string for DuchyFramework.
+FOUNDATION_EXPORT const unsigned char DuchyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DuchyFramework/PublicHeader.h>
+
+
---
+++ DuchyFrameworkTests/DuchyFrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  DuchyFrameworkTests.swift
+//  DuchyFrameworkTests
+//
+//  Created by Matt Glover on 25/08/2016.
+//  Copyright © 2016 Duchy Software. All rights reserved.
+//
+
+import XCTest
+@testable import DuchyFramework
+
+class DuchyFrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
